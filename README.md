1243cee058df8acadbfabda4039cd4a82ae4ae07
Giraldo Rosales <giraldo@nitrogenlabs.com>
2014-04-11 20:53:38 -0400

    Documentation added for server

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ npm test
 ```js
 var Oriento =  require('oriento');
 
-var server = Oriento({
+var server = new Oriento({
   host: 'localhost',
   port: 2424,
   username: 'root',
@@ -68,7 +68,7 @@ You should carefully benchmark this against the default setting for your use cas
 there are scenarios where a connection pool is actually slightly worse for performance than a single connection.
 
 ```js
-var server = Oriento({
+var server = new Oriento({
   host: 'localhost',
   port: 2424,
   username: 'root',
--- test/server/server-test.js
+++ test/server/server-test.js
@@ -1,101 +1,103 @@
+'use strict';
+
+/* global TEST_SERVER, LIB */
 var errors = LIB.errors;
 
-
-describe("Server", function () {
-  describe('Server::connect()', function () {
-    it("should negotiate a connection", function () {
+describe('Server', function() {
+  describe('Server::connect()', function() {
+    it('should negotiate a connection', function() {
       return TEST_SERVER.connect()
-      .then(function (server) {
-        server.sessionId.should.be.above(-1);
-      });
+        .then(function(server) {
+          server.sessionId.should.be.above(-1);
+        });
     });
   });
-  describe('Server::send()', function  () {
-    it("should handle errors correctly", function () {
+  describe('Server::send()', function() {
+    it('should handle errors correctly', function() {
       return TEST_SERVER.send('db-open', {
         name: 'not_an_existing_database',
         type: 'graph',
         username: 'admin',
         password: 'admin'
       })
-      .then(function (response) {
-        throw new Error('Should Not Happen!');
-      })
-      .catch(errors.Request, function (e) {
-        e.type.should.equal('com.orientechnologies.orient.core.exception.OConfigurationException');
-        return true;
-      });
-    })
+        .then(function(response) {
+          throw new Error('Should Not Happen!');
+        })
+        .catch(errors.Request, function(e) {
+          e.type.should.equal('com.orientechnologies.orient.core.exception.OConfigurationException');
+          return true;
+        });
+    });
   });
 });
-describe('Server::create()', function () {
-  it("should create a new database", function () {
+describe('Server::create()', function() {
+  it('should create a new database', function() {
     return TEST_SERVER.create({
       name: 'testdb_server',
       type: 'graph',
       storage: 'memory'
     })
-    .then(function (db) {
-      db.name.should.equal('testdb_server');
-    });
+      .then(function(db) {
+        db.name.should.equal('testdb_server');
+      });
   });
 });
-describe('Server::list()', function () {
-  it("should list the existing databases", function () {
+describe('Server::list()', function() {
+  it('should list the existing databases', function() {
     return TEST_SERVER.list()
-    .then(function (dbs) {
-      var names = Object.keys(dbs),
+      .then(function(dbs) {
+        var names = Object.keys(dbs),
           total = names.length,
           i, db, name;
-      for (i = 0; i < total; i++) {
-        name = names[i];
-        db = dbs[name];
-        db.should.be.an.instanceOf(LIB.Db);
-      }
-      dbs.testdb_server.storage.should.equal('memory');
-    });
+        for(i = 0; i<total; i++) {
+          name = names[i];
+          db = dbs[name];
+          db.should.be.an.instanceOf(LIB.Db);
+        }
+        dbs.testdb_server.storage.should.equal('memory');
+      });
   });
 });
-describe('Server::exists()', function () {
-  it("should confirm an existing database exists", function () {
+describe('Server::exists()', function() {
+  it('should confirm an existing database exists', function() {
     return TEST_SERVER.exists('testdb_server')
-    .then(function (exists) {
-      exists.should.be.true;
-    });
+      .then(function(exists) {
+        exists.should.be.true;
+      });
   });
-  it("should confirm a missing database does not exist", function () {
+  it('should confirm a missing database does not exist', function() {
     return TEST_SERVER.exists('a_missing_database')
-    .then(function (exists) {
-      exists.should.be.false;
-    });
+      .then(function(exists) {
+        exists.should.be.false;
+      });
   });
 });
-describe('Server::delete()', function () {
-  it("should delete a database", function () {
+describe('Server::delete()', function() {
+  it('should delete a database', function() {
     return TEST_SERVER.delete({
       name: 'testdb_server',
       type: 'graph',
       storage: 'memory'
     })
-    .then(function (response) {
-      response.should.be.true;
-    });
+      .then(function(response) {
+        response.should.be.true;
+      });
   });
 });
-describe('Server::config.list', function () {
-  it("should list the server config", function () {
+describe('Server::config.list', function() {
+  it('should list the server config', function() {
     return TEST_SERVER.config.list()
-    .then(function (config) {
-      config.should.have.property('db.pool.min');
-    });
+      .then(function(config) {
+        config.should.have.property('db.pool.min');
+      });
   });
 });
 
-describe('Server::config.get', function () {
-  it("should get a server config key", function () {
+describe('Server::config.get', function() {
+  it('should get a server config key', function() {
     return TEST_SERVER.config.get('db.pool.min')
-    .then(function (value) {
-      value.should.have.type('string');
-    });
+      .then(function(value) {
+        value.should.have.type('string');
+      });
   });
 });
