2255376c10bdfe1387aaf2b64eeffe62a8f547b9
Giraldo Rosales <giraldo@nitrogenlabs.com>
2014-04-23 11:16:49 -0400

    Performance tweaks and Cleanup

---
+++ test/bugs/25-property-create.js
@@ -0,0 +1,31 @@
+'use strict';
+/* global before, after, CREATE_TEST_DB, DELETE_TEST_DB */
+
+describe('Bug #25: Create undefined in Myclass.property.create', function () {
+  before(function () {
+    return CREATE_TEST_DB(this, 'testdb_bug_25')
+      .bind(this)
+      .then(function () {
+        return this.db.class.create('Member', 'V');
+      })
+      .then(function (item) {
+        this.class = item;
+      });
+  });
+  after(function () {
+    return DELETE_TEST_DB('testdb_bug_25');
+  });
+
+  it('Let me create a property immediately after creating a class', function () {
+    var values = {
+      name: 'name',
+      type: 'String',
+      mandatory: true,
+      max: 65
+    };
+    return this.class.property.create(values)
+      .then(function (prop) {
+        prop.should.have.properties(values);
+      });
+  });
+});
