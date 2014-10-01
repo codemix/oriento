e84047257bd54978728746a3a0ef87897b35bf08
Charles Pick <charles.pick@gmail.com>
2014-10-01 17:05:22 +0100

    alternative fix #129

--- test/core/bag-test.js
+++ test/core/bag-test.js
@@ -1,109 +1,72 @@
 describe("RID Bag", function () {
-    describe('Embedded Bag Query', function () {
-        before(function () {
-          var self = this;
-          return CREATE_TEST_DB(this, 'testdb_dbapi_rid_bag_embedded_query')
-          .bind(this)
-          .then(function () {
-            return this.db.class.create('Person', 'V');
-          })
-          .then(function (Person) {
-            this.Person = Person;
-            return this.db.class.create('Knows', 'E');
-          })
-          .then(function (Knows) {
-            this.Knows = Knows;
-            return this.Person.create({
-              name: 'John Smith'
-            });
-          })
-          .then(function (subject) {
-            var limit = 10,
-                i;
-            this.subject = subject;
-            this.people = [];
-            for (i = 0; i < limit; i++) {
-              this.people.push({
-                name: 'Friend ' + i
-              });
-            }
-            return this.Person.create(this.people);
-          })
-          .then(function () {
-            return this.db.edge
-            .from(this.subject['@rid'])
-            .to('SELECT * FROM Person WHERE name LIKE "Friend%"')
-            .create('Knows');
-          });
+  before(function () {
+    var self = this;
+    return CREATE_TEST_DB(this, 'testdb_dbapi_rid_bag_embedded')
+    .bind(this)
+    .then(function () {
+      return this.db.class.create('Person', 'V');
+    })
+    .then(function (Person) {
+      this.Person = Person;
+      return this.db.class.create('Knows', 'E');
+    })
+    .then(function (Knows) {
+      this.Knows = Knows;
+      return this.Person.create({
+        name: 'John Smith'
+      });
+    })
+    .then(function (subject) {
+      var limit = 10,
+          i;
+      this.subject = subject;
+      this.people = [];
+      for (i = 0; i < limit; i++) {
+        this.people.push({
+          name: 'Friend ' + i
         });
-        after(function () {
-          return DELETE_TEST_DB('testdb_dbapi_rid_bag_embedded_query');
-        });
+      }
+      return this.Person.create(this.people);
+    })
+    .then(function () {
+      return this.db.edge
+      .from(this.subject['@rid'])
+      .to('SELECT * FROM Person WHERE name LIKE "Friend%"')
+      .create('Knows');
+    });
+  });
+  after(function () {
+    return DELETE_TEST_DB('testdb_dbapi_rid_bag_embedded');
+  });
 
-        beforeEach(function () {
-          return this.db
-            .select()
-            .from(this.subject['@rid'])
-            .fetch({'*': 2})
-            .one()
-            .bind(this)
-            .then(function (record) {
-              this.resolvedBag = record.out_Knows;
-            });
+  describe('Embedded Bag Query', function () {
+    beforeEach(function () {
+      return this.db
+        .select()
+        .from(this.subject['@rid'])
+        .fetch({'*': 2})
+        .one()
+        .bind(this)
+        .then(function (record) {
+          this.resolvedBag = record.out_Knows;
         });
+    });
 
-        it('should resolve rids in bag', function () {
-          this.resolvedBag.should.be.an.instanceOf(Array)
-          this.resolvedBag[0]['@class'].should.equal('Person');
-        });
-    }); 
+    it('should resolve rids in bag', function () {
+      this.resolvedBag.next()['@class'].should.equal('Person');
+    });
+  });
 
   describe('Embedded Bag', function () {
-    before(function () {
-      var self = this;
-      return CREATE_TEST_DB(this, 'testdb_dbapi_rid_bag_embedded')
-      .bind(this)
-      .then(function () {
-        return this.db.class.create('Person', 'V');
-      })
-      .then(function (Person) {
-        this.Person = Person;
-        return this.db.class.create('Knows', 'E');
-      })
-      .then(function (Knows) {
-        this.Knows = Knows;
-        return this.Person.create({
-          name: 'John Smith'
-        });
-      })
-      .then(function (subject) {
-        var limit = 10,
-            i;
-        this.subject = subject;
-        this.people = [];
-        for (i = 0; i < limit; i++) {
-          this.people.push({
-            name: 'Friend ' + i
-          });
-        }
-        return this.Person.create(this.people);
-      })
-      .then(function () {
-        return this.db.edge
-        .from(this.subject['@rid'])
-        .to('SELECT * FROM Person WHERE name LIKE "Friend%"')
-        .create('Knows');
-      });
-    });
-    after(function () {
-      return DELETE_TEST_DB('testdb_dbapi_rid_bag_embedded');
-    });
-
     beforeEach(function () {
-      return this.db.exec("select from " + this.subject['@rid'] + ' fetchPlan *:1')
+      return this.db
+      .select()
+      .from(this.subject['@rid'])
+      .fetch({'*': 2})
+      .one()
       .bind(this)
-      .then(function (response) {
-        this.bag = response.results[0].content[0].value.out_Knows;
+      .then(function (record) {
+        this.bag = record.out_Knows;
       });
     });
 
@@ -119,7 +82,7 @@ describe("RID Bag", function () {
           i = 0,
           item;
       while((item = this.bag.next())) {
-        item.should.be.an.instanceOf(LIB.RID);
+        (item['@rid'] || item).should.be.an.instanceOf(LIB.RID);
         i++;
       }
       i.should.equal(10);
@@ -186,10 +149,14 @@ describe("RID Bag", function () {
     });
 
     beforeEach(function () {
-      return this.db.exec("select from " + this.subject['@rid'] + ' fetchPlan *:1')
+      return this.db
+      .select()
+      .from(this.subject['@rid'])
+      .fetch({'*': 2})
+      .one()
       .bind(this)
-      .then(function (response) {
-        this.bag = response.results[0].content[0].value.out_Knows;
+      .then(function (record) {
+        this.bag = record.out_Knows;
       });
     });
 
