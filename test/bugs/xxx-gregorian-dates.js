16ca07e6fc1f019731215c27587a25e9df6c6b02
Charles Pick <charles.pick@gmail.com>
2014-12-19 15:58:05 +0000

    replicate date parsing issue [ci skip]

--- test/bugs/xxx-gregorian-dates.js
+++ test/bugs/xxx-gregorian-dates.js
@@ -1,4 +1,4 @@
-describe("Bug XXX: Gregorian dates", function () {
+describe.only("Bug XXX: Gregorian dates", function () {
   var rid;
   before(function () {
     return CREATE_TEST_DB(this, 'testdb_bug_gregorian_dates')
@@ -22,21 +22,38 @@ describe("Bug XXX: Gregorian dates", function () {
           type: 'datetime'
         }
       ]);
-    })
-    .then(function () {
-      return this.db.insert().into('TestClass').set({name: 'test', someDate: new Date("1582-10-04"), someDateTime: new Date("1582-10-14 10:00:00")}).one();
     });
   });
   after(function () {
     return DELETE_TEST_DB('testdb_bug_gregorian_dates');
   });
 
-  it('should store the correct date + datetime', function () {
-    return this.db.select().from('TestClass').one()
-    .then(function (item) {
-      item.name.should.equal('test');
-      item.someDate.should.eql(new Date("1582-10-04"));
-      item.someDateTime.should.eql(new Date("1582-10-14 10:00:00"));
-    })
+  describe('Using Bound Parameters', function () {
+    before(function () {
+      return this.db.insert().into('TestClass').set({name: 'test', someDate: new Date("1582-10-04"), someDateTime: new Date("1582-10-14 10:00:00")}).one();
+    });
+    it('should store the correct date + datetime', function () {
+      return this.db.select().from('TestClass').one()
+      .then(function (item) {
+        item.name.should.equal('test');
+        item.someDate.should.eql(new Date("1582-10-04"));
+        item.someDateTime.should.eql(new Date("1582-10-14 10:00:00"));
+      })
+    });
+  });
+
+  describe('Using Vanilla Queries', function () {
+    before(function () {
+      var query = this.db.insert().into('TestClass').set({name: 'testzzzz', someDate: new Date("1582-10-04"), someDateTime: new Date("1582-10-14 10:00:00")}).toString();
+      return this.db.query(query);
+    });
+    it('should store the correct date + datetime', function () {
+      return this.db.select().from('TestClass').one()
+      .then(function (item) {
+        item.name.should.equal('test');
+        item.someDate.should.eql(new Date("1582-10-04"));
+        item.someDateTime.should.eql(new Date("1582-10-14 10:00:00"));
+      })
+    });
   });
 });
